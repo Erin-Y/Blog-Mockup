7401e800ca815bf08d468fc8d3d787a7ae87ff76
Yiran Yan <erin.yyr@gmail.com>
2018-10-28 01:13:21 -0700

    Added fields into the form and added validate messages

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input {...field.input} />
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
       </div>
     );
   }
@@ -13,12 +14,39 @@ class PostsNew extends Component {
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderTitleField} />
+        <Field label="Post Title" name="title" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  } else if (values.title.length < 3) {
+    errors.title = "Title must have least 3 characters!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "PostNewForm"
 })(PostsNew);
